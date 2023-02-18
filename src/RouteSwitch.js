05a815942ce0c43bc06438b5a9f1488392e1d703
Daniel Burgess <37884090+Danb981@users.noreply.github.com>
2023-02-18 16:04:00 -0500

    package.json updated

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Checkout from "./Checkout";
 import OrderConfirmation from "./OrderConfirmation";
 
 function RouteSwitch() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="orderconfirmation" element={<OrderConfirmation />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
